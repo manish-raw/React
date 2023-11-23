6c02496bbc4b44ec1b89cf9435ff65fbb57576ce
manish-raw <manishyadav.y31@gmail.com>
2023-11-23 20:05:55 +0530

    added password generator logic

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -8,13 +8,50 @@ function App() {
   const [password, setPassword] = useState("");
 
   const passwordGenerator = useCallback( ()=> {
+    let passwordCarrier="";
+    let str="ABCDEFGHIJKLMNOPQRESTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if(charAllowed) str +='~!@#$%^&*()_-+{}[]/';
+    if(numberAllowed) str+="0123456789";
+    for (let i = 0; i< length; i++) {
+      const index = Math.floor( Math.random() * str.length() +1 );
+      passwordCarrier += str.charAt(index);
+      
+    }
     
-  }, 
-    [length, numberAllowed, charAllowed]);
+  }, [length, numberAllowed, charAllowed, setPassword]
+  );
 
   return (
     <>
-      <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+      <div className='text-white max-w-md mx-auto px-4 py-3 my-8 rounded-lg bg-gray-600'>
+        <h1 className='text-center text-white text-2xl my-4'>Password Generator</h1>
+        <div className='flex rounded-lg overflow-hidden shadow mb-4' >
+            <input 
+            type="text" 
+            value={password} 
+            className='outline-none w-full py-1 px-3'
+            placeholder='password'
+            readOnly
+            />
+            <button className='outline-none bg-blue-500 p-1 text-white' >Copy</button>
+        </div>
+
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input 
+            type="range"
+            min={5}
+            max={20}
+            value={length}
+            className='cursor-pointer'
+            onChange={ (e)=> {setlength(e.target.value)} }
+            />
+            <label>Length: {length}</label>
+            <input type="" />
+          </div>
+        </div>
+
+      </div>
     </>
   )
 }
