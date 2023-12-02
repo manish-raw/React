150a6fe430653970c6ee2e6fb1ebd0317da41c26
manish-raw <manishyadav.y31@gmail.com>
2023-12-02 22:49:32 +0530

    Implemented add and remove todo

---
+++ TodoApp/src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from "./components/AddTodo"
+import Todos from "./components/Todos"
+
+
+function App() {
+
+  return (
+    <>
+    <h1>This Todo App is created while learning Redux Toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
