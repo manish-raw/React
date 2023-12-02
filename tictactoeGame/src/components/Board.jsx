719d0041223306d0aa9a25c7dd4cdacbc1c5fdea
manish-raw <manishyadav.y31@gmail.com>
2023-12-02 14:31:25 +0530

    winner logic added

--- tictactoeGame/src/components/Board.jsx
+++ tictactoeGame/src/components/Board.jsx
@@ -3,9 +3,10 @@
  export default function Board(){
     const [nextTurn, setNextTurn] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
+    
 
     function handleClick(i){
-        if(squares[i]){
+        if(squares[i] || calculateWinner(squares)){
             return
         }
         const newSquare = squares.slice();
@@ -18,8 +19,17 @@
         setNextTurn(!nextTurn)
     }
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        status = "Player turn: " + (nextTurn ? "X" : "O");
+    }
+
     return (
         <>
+            <div className="status">{status}</div>
             <div className="board-row">
                 <Square value={squares[0]} onSquareClick={()=> handleClick(0)} />
                 <Square value={squares[1]} onSquareClick={()=> handleClick(1)} />
@@ -38,4 +48,25 @@
             
         </>
     )
+
+
+    function calculateWinner(squares) {
+        const lines = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6]
+        ];
+        for (let i = 0; i < lines.length; i++) {
+          const [a, b, c] = lines[i];
+          if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+          }
+        }
+        return null;
+      }
  }
